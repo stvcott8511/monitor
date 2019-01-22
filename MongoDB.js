f03ff9a62c07e1f49e4a6303e85e711458d5002e
Steven <stvcott8511@gmail.com>
2019-01-22 15:34:04 -0500

    Updates to MongoDB.js

--- MongoDB.js
+++ MongoDB.js
@@ -3,31 +3,32 @@ const dbLib = require( __dirname + "/dbUtils" );
 const _ = require( "lodash" );
 
 class MongoDB {
-    constructor( url, dbName, collection, logger ) {
-        this.url = url;
-        this.dbName = dbLib;
+    constructor( dbHost, port, dbName, collection, logger ) {
+        this.dbHost = dbHost;
+        this.port = port;
+        this.dbName = dbName;
         this.collection = collection;
         this.logger = logger;
     }
     connect() {
-        mongo.connect( this.url, this.dbName, ( err, db ) => {
-            return new Promise( ( resolve, reject ) => {
-                if ( _.isUndefined( db ) ) { 
-                    this.logger.log( `Connected to MongoDB: ${this.url}` );
-                    resolve( db );
-                } else {
-                    this.logger.log( `Connection error to MongoDB: ${this.url}` );
-                    //TODO error logging
+        return new Promise( ( resolve, reject ) => {
+            let url = `${this.dbHost}:${this.port}`;
+            mongo.connect( url, { useNewUrlParser: true }, ( err, db ) => {
+                if ( _.isNull( db ) ) { 
+                    this.logger.log( `Connection error to MongoDB Host:${this.dbHost}, Port:${this.port}` );
                     reject( err );
-                }
+                    return;
+                } 
+                this.logger.log( `Connected to MongoDB: ${this.dbHost}` );
+                resolve( db );
             } );
         } );
     }
     find( data ) {
         return new Promise( ( resolve, reject ) => {
-            this.connect.then( ( db ) => {
-                db.collection( this.collection ).find.toArray( data, ( err, result ) => {
-                    if( !_.isUndefined( err ) ) {
+            this.connect().then( ( db ) => {
+                db.db( this.dbName ).collection( this.collection ).find.toArray( data, ( err, result ) => {
+                    if( _.isNull( result ) ) {
                         this.logger.log( `Find to MongoDB collection: ${this.collection}` );
                         // TODO error logging
                         reject( err );
@@ -43,9 +44,9 @@ class MongoDB {
     }
     insert( data ) {
         return new Promise( ( resolve, reject ) => {
-            this.connect.then( ( db ) => {
-                db.collection( this.collection ).insertOne( data, ( err, result ) => {
-                    if( !_.isUndefined( err ) ) {
+            this.connect().then( ( db ) => {
+                db.db( this.dbName ).collection( this.collection ).insertOne( data, ( err, result ) => {
+                    if( _.isNull( result ) ) {
                         this.logger.log( `Insertion failed to MongoDB collection: ${this.collection}` );
                         // TODO error logging
                         reject( err );
@@ -62,8 +63,8 @@ class MongoDB {
     update( data ) {
         return new Promise( ( resolve, reject ) => {
             this.connect.then( ( db ) => {
-                db.collection( this.collection ).updateOne( data, ( err, result ) => {
-                    if( !_.isUndefined( err ) ) {
+                db.db( this.dbName ).collection( this.collection ).updateOne( data, ( err, result ) => {
+                    if( _.isNull( result ) ) {
                         this.logger.log( `Update failed to MongoDB collection: ${this.collection}` );
                         // TODO error logging
                         reject( err );
@@ -79,9 +80,9 @@ class MongoDB {
     }
     delete( data ) {
         return new Promise( ( resolve, reject ) => {
-            this.connect.then( ( db ) => {
-                db.collection( this.collection ).deleteOne( data, ( err, result ) => {
-                    if( !_.isUndefined( err ) ) {
+            this.connect().then( ( db ) => {
+                db.db( this.dbName ).collection( this.collection ).deleteOne( data, ( err, result ) => {
+                    if( _.isNull( result ) ) {
                         this.logger.log( `Update failed to MongoDB collection: ${this.collection}` );
                         // TODO error logging
                         reject( err );
