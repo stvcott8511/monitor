a8c5375f2b0a6044d26d874a1b3ec466d9de9177
stv <stv@stv-VirtualBox18.04>
2019-01-26 15:48:14 -0500

    Fixed bugs cleanup

--- MongoDB.js
+++ MongoDB.js
@@ -13,29 +13,28 @@ class MongoDB {
     connect() {
         return new Promise( ( resolve, reject ) => {
             let url = `${this.dbHost}:${this.port}`;
-            mongo.connect( url, { useNewUrlParser: true }, ( err, db ) => {
-                if ( _.isNull( db ) ) { 
-                    this.logger.log( `Connection error to MongoDB Host:${this.dbHost}, Port:${this.port}` );
+            mongo.connect( url, { useNewUrlParser: true }, ( err, conn ) => {
+                if ( _.isNull( conn ) || _.isUndefined( conn ) ) { 
+                    this.logger.error( `Connection error to MongoDB on Host:${this.dbHost}, Port:${this.port}` );
                     reject( err );
                     return;
                 } 
-                this.logger.log( `Connected to MongoDB: ${this.dbHost}` );
-                resolve( db );
+                this.logger.log( `Connected to MongoDB on Host: ${this.dbHost}, Port:${this.port}` );
+                resolve( conn );
             } );
         } );
     }
-    find( data ) {
+    find( query ) {
         return new Promise( ( resolve, reject ) => {
-            this.connect().then( ( db ) => {
-                db.db( this.dbName ).collection( this.collection ).find.toArray( data, ( err, result ) => {
-                    if( _.isNull( result ) ) {
-                        this.logger.log( `Find to MongoDB collection: ${this.collection}` );
-                        // TODO error logging
+            this.connect().then( ( conn ) => {
+                conn.db( this.dbName ).collection( this.collection ).find( query ).toArray( ( err, result ) => {
+                    if( _.isNull( result ) || _.isUndefined( result ) ) {
+                        this.logger.error( `Find to MongoDB collection: ${this.collection}` );
                         reject( err );
                     } else {
                         resolve( result );
                     }
-                    db.close();
+                    conn.close();
                 } );
             } ).catch( ( err ) => {
                 reject( err );
@@ -44,52 +43,49 @@ class MongoDB {
     }
     insert( data ) {
         return new Promise( ( resolve, reject ) => {
-            this.connect().then( ( db ) => {
-                db.db( this.dbName ).collection( this.collection ).insertOne( data, ( err, result ) => {
-                    if( _.isNull( result ) ) {
-                        this.logger.log( `Insertion failed to MongoDB collection: ${this.collection}` );
-                        // TODO error logging
+            this.connect().then( ( conn ) => {
+                conn.db( this.dbName ).collection( this.collection ).insertOne( data, ( err, result ) => {
+                    if( _.isNull( result ) || _.isUndefined( result ) ) {
+                        this.logger.error( `Insertion failed to MongoDB collection: ${this.collection}` );
                         reject( err );
                     } else {
                         resolve( result );
                     };
-                    db.close();
+                    conn.close();
                 } );
             } ).catch( ( err ) => {
                 reject( err );
             } );
         } );
     }
-    update( data ) {
+    update( query, data ) {
         return new Promise( ( resolve, reject ) => {
-            this.connect.then( ( db ) => {
-                db.db( this.dbName ).collection( this.collection ).updateOne( data, ( err, result ) => {
-                    if( _.isNull( result ) ) {
-                        this.logger.log( `Update failed to MongoDB collection: ${this.collection}` );
-                        // TODO error logging
+            this.connect().then( ( conn ) => {
+                conn.db( this.dbName ).collection( this.collection ).updateOne( query, data, ( err, result ) => {
+                    if( _.isNull( result ) || _.isUndefined( result ) ) {
+                        this.logger.error( `Update failed to MongoDB collection: ${this.collection}` );
                         reject( err );
                     } else {
                         resolve( result );
                     };
-                    db.close();
+                    conn.close();
                 } );
             } ).catch( ( err ) => {
                 reject( err );
             } );
         } );
     }
-    delete( data ) {
+    delete( query ) {
         return new Promise( ( resolve, reject ) => {
-            this.connect().then( ( db ) => {
-                db.db( this.dbName ).collection( this.collection ).deleteOne( data, ( err, result ) => {
-                    if( _.isNull( result ) ) {
-                        this.logger.log( `Update failed to MongoDB collection: ${this.collection}` );
-                        // TODO error logging
+            this.connect().then( ( conn ) => {
+                conn.db( this.dbName ).collection( this.collection ).deleteOne( query, ( err, result ) => {
+                    if( _.isNull( result ) || _.isUndefined( result ) ) {
+                        this.logger.error( `Update failed to MongoDB collection: ${this.collection}` );
                         reject( err );
                     } else {
                         resolve( result );
                     };
-                    db.close();
+                    conn.close();
                 } );
             } ).catch( ( err ) => {
                 reject( err );
--- dbManager.js
+++ dbManager.js
@@ -1,4 +1,5 @@
-const MongoDB = require( "MongoDB" );
+const MongoDB = require( __dirname + "/MongoDB" );
+const _ = require( "lodash" );
 
 var databases = {};
 
@@ -7,10 +8,13 @@ function getMongoDBConfig() {
     let config = {
         dbHost: "mongodb://localhost",
         port: 27017,
+        dbname: "mondb",
         collection: "monitor",
+        dbClass: MongoDB
     }
-    return new MongoDB( `${config.dbHost}:${config.port}/${config.collection}` );
+    return config;
 }
+
 // Add more DBs later
 databases["MongoDB"] = getMongoDBConfig();
 
@@ -25,9 +29,22 @@ class DBManager {
 
 let instance = undefined;
 
-module.exports = function( name ) {
-    if( instance ) {
+module.exports = function( name, logger, config ) { 
+    if( _.isUndefined( instance ) ) {
         instance = new DBManager();
     }
-    return instance.getDatabase( name );
+    
+    let baseConfig;
+    try {
+        baseConfig = instance.getDatabase( name );
+    } catch ( err ) {
+        new Error( `No config for database type: ${name}` );
+    }
+
+    baseConfig.dbHost = _.get( config,  "dbHost" , baseConfig.dbHost );
+    baseConfig.port = _.get( config, "port" , baseConfig.port );
+    baseConfig.dbname = _.get( config, "dbname" , baseConfig.dbname );
+    baseConfig.collection = _.get( config, "collection", baseConfig.collection );
+
+    return new baseConfig.dbClass(  baseConfig.dbHost, baseConfig.port, baseConfig.dbname, baseConfig.collection, logger );
 }
