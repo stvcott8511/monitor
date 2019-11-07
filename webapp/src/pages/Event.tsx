fcc46ab921ad9c72b8f826f4bb467f14a8d33313
Christopher Kuo <kuo.christopher.c@gmail.com>
2019-11-06 20:48:31 -0500

    WIP: Stub line charts for all log severity types

--- webapp/src/pages/Event.tsx
+++ webapp/src/pages/Event.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 import LineChart from '../components/core/Charts/LineChart';
 
+const CHART_LABELS = Object.freeze(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']);
+
 export interface EventRouteParams {
     monitorName: string;
     eventType: string;
@@ -21,10 +23,30 @@ const Event: React.FunctionComponent<EventProps> = (props) => {
     return (
         <Grid container>
             <Grid item xs={3}>
-                <LineChart
-                    label="Active Users"
-                    chartLabels={['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']}
-                    data={[542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630]} />
+                <div>
+                    <LineChart
+                        label="Active Users"
+                        chartLabels={[...CHART_LABELS]}
+                        data={[542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630]} />
+                </div>
+                <div>
+                    <LineChart
+                        label="Active Users"
+                        chartLabels={[...CHART_LABELS]}
+                        data={[542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630]} />
+                </div>
+                <div>
+                    <LineChart
+                        label="Active Users"
+                        chartLabels={[...CHART_LABELS]}
+                        data={[542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630]} />
+                </div>
+                <div>
+                    <LineChart
+                        label="Active Users"
+                        chartLabels={[...CHART_LABELS]}
+                        data={[542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 630]} />
+                </div>
             </Grid>
             <Grid item xs={9}>
 
