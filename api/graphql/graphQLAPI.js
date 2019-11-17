ba4d2d54ba5de9c26e448cad7b04e5aa9812e673
Steven <stvcott8511@gmail.com>
2019-11-17 16:51:09 -0500

    Start mongdb support for GraphQL

--- api/graphql/graphQLAPI.js
+++ api/graphql/graphQLAPI.js
@@ -1,22 +1,13 @@
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 const rootSchemaMon = require( __dirname + "/schemas/monitor/rootMonitorQuery" );
-const rootSchemaEvent = require( __dirname + "/schemas/event/rootEventQuery" );
-
-let schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-let root = { hello: () => 'Hello world!' };
-
-let expressModule = graphqlHTTP( {
-  schema: rootSchemaMon,
-  //rootValue: root,
-  graphiql: true
-} );
 
 module.exports = ( app, logger ) => {
+  let expressModule = graphqlHTTP( {
+    schema: rootSchemaMon( logger ),
+    //rootValue: root,
+    graphiql: true
+  } );
   logger.log( "Applying graphql hook" );
   app.use('/graphql', expressModule );
 };
--- api/graphql/schemas/monitor/rootMonitorQuery.js
+++ api/graphql/schemas/monitor/rootMonitorQuery.js
@@ -1,21 +1,54 @@
 const graphql = require( "graphql" );
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLList } = graphql;
 const MonitorType = require( __dirname + "/Monitor" );
+const _ = require( "lodash" );
+const dbManager = require( __dirname + "/../../../database/dbManager" );
 
-const RootQuery = new GraphQLObjectType( {
-    name: "RootQueryType",
-    fields: {
-        monitor: {
-            type: MonitorType,
-            args: { _id: { type: GraphQLString } },
-            resolve( parent, args ) {
-                //todo
-                debugger;
+// Test data for now
+let monitors = [ 
+    { 
+       "_id":"5dacb79c93ad26692d2d208f",
+       "monName":"mon1",
+       "description":"Testing monitor db collection",
+       "timeStamp":"2019-10-20T15:38:04-04:00"
+    },
+    { 
+       "_id":"5dacb7b093ad26692d2d2090",
+       "monName":"mon2",
+       "description":"Testing monitor db collection",
+       "timeStamp":"2019-10-20T15:38:24-04:00"
+    }
+];
+
+let qlRoot = ( logger ) => {
+    let db = dbManager( "Defualt", logger, "monitors" );
+    const RootQuery = new GraphQLObjectType( {
+        name: "RootQueryType",
+        fields: {
+            monitor: {
+                type: MonitorType,
+                args: { _id: { type: GraphQLString } },
+                resolve( parent, args ) {
+                    return _.find( monitors, { _id: args._id } );
+                }
+            },
+            monitors: {
+                type: new GraphQLList( MonitorType ),
+                async resolve( parent, args ) {
+                    try {
+                        return await db.find( {} );
+                    } catch (err) {
+                        logger.log( "GraphQL query failed" );
+                        return [];
+                    }
+                }
             }
         }
-    }
-} );
+    } );
 
-module.exports = new GraphQLSchema( {
-    query: RootQuery
-} );
+    return new GraphQLSchema( {
+        query: RootQuery
+    } );
+}
+
+module.exports = qlRoot;
