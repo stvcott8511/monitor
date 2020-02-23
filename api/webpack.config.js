8cba7ff127bd877f508e6c1ea5fd3f3f10edb5fb
Christopher Kuo <kuo.christopher.c@gmail.com>
2020-02-23 18:26:54 -0500

    WIP: Add packages for TypeScript

---
+++ api/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const {
+  NODE_ENV = 'production',
+} = process.env;
+module.exports = {
+  entry: './server.js',
+  mode: NODE_ENV,
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.mjs', '.js'],
+  }
+}
