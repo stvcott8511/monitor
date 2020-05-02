e363aaa519f34ef329290d1e96b4091c3fb12b27
Steven Cottier <stvcott8511@gmail.com>
2020-05-02 13:29:43 -0400

    Updated settings file with default port rabbitmq

--- api/docker/startdocker.sh
+++ api/docker/startdocker.sh
@@ -5,10 +5,17 @@ if [ "$1" == "rabbitmq_mongo_server" ]; then
         rm -rf dockerbuild
         mkdir dockerbuild
         npm i
-        npm run build
+        if [ "$3" == "dev" ]
+        then
+            echo "Starting webpack dev build"
+            npm run build:dev
+        else
+            npm run build
+        fi
         cp rabbitmq_mongo_server/Dockerfile dockerbuild/Dockerfile
         cp ../config/settings.env dockerbuild/
         cp ../build/index.js dockerbuild/
+        cp ../build/index.js.map dockerbuild/
         cp rabbitmq_mongo_server/start.sh dockerbuild/
         cd dockerbuild
         docker build -t $2 .
