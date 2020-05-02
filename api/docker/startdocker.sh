e7583196fc1b847598222a90b95a728e30021508
Steven <stvcott8511@gmail.com>
2020-05-02 11:44:19 -0400

    Updated shell script ext from bash to sh

--- api/docker/startdocker.sh
+++ api/docker/startdocker.sh
@@ -4,11 +4,12 @@ if [ "$1" == "rabbitmq_mongo_server" ]; then
     if [ "$2" != "" ]; then
         rm -rf dockerbuild
         mkdir dockerbuild
+        npm i
         npm run build
         cp rabbitmq_mongo_server/Dockerfile dockerbuild/Dockerfile
         cp ../config/settings.env dockerbuild/
         cp ../build/index.js dockerbuild/
-        cp rabbitmq_mongo_server/start.bash dockerbuild/
+        cp rabbitmq_mongo_server/start.sh dockerbuild/
         cd dockerbuild
         docker build -t $2 .
     else
